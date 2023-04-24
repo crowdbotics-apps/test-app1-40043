af4df6be0dd46c81958f0e0fbe36b06e4566b5ef
Mohammad Lucky <mohammad.lucky.dms@crowdbotics.com>
2023-04-24 14:41:18 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- store/testappAPI/api.js
+++ store/testappAPI/api.js
@@ -1,50 +1,72 @@
-import axios from "axios"
+import axios from "axios";
 const testappAPI = axios.create({
   baseURL: "https://test-app1-40043-prod.herokuapp.com",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+
 function api_v1_login_create(payload) {
-  return testappAPI.post(`/api/v1/login/`, payload.data)
+  return testappAPI.post(`/api/v1/login/`, payload.data);
 }
+
 function api_v1_signup_create(payload) {
-  return testappAPI.post(`/api/v1/signup/`, payload.data)
+  return testappAPI.post(`/api/v1/signup/`, payload.data);
 }
+
 function rest_auth_user_retrieve(payload) {
-  return testappAPI.get(`/rest-auth/user/`)
+  return testappAPI.get(`/rest-auth/user/`);
 }
+
 function rest_auth_user_update(payload) {
-  return testappAPI.put(`/rest-auth/user/`, payload.data)
+  return testappAPI.put(`/rest-auth/user/`, payload.data);
 }
+
 function rest_auth_user_partial_update(payload) {
-  return testappAPI.patch(`/rest-auth/user/`, payload.data)
+  return testappAPI.patch(`/rest-auth/user/`, payload.data);
 }
+
 function api_docs_schema_retrieve(payload) {
-  return testappAPI.get(`/api-docs/schema/`, { params: { lang: payload.lang } })
+  return testappAPI.get(`/api-docs/schema/`, {
+    params: {
+      lang: payload.lang
+    }
+  });
 }
+
 function rest_auth_login_create(payload) {
-  return testappAPI.post(`/rest-auth/login/`, payload.data)
+  return testappAPI.post(`/rest-auth/login/`, payload.data);
 }
+
 function rest_auth_logout_retrieve(payload) {
-  return testappAPI.get(`/rest-auth/logout/`)
+  return testappAPI.get(`/rest-auth/logout/`);
 }
+
 function rest_auth_logout_create(payload) {
-  return testappAPI.post(`/rest-auth/logout/`)
+  return testappAPI.post(`/rest-auth/logout/`);
 }
+
 function rest_auth_registration_create(payload) {
-  return testappAPI.post(`/rest-auth/registration/`, payload.data)
+  return testappAPI.post(`/rest-auth/registration/`, payload.data);
 }
+
 function rest_auth_password_reset_create(payload) {
-  return testappAPI.post(`/rest-auth/password/reset/`, payload.data)
+  return testappAPI.post(`/rest-auth/password/reset/`, payload.data);
 }
+
 function rest_auth_password_change_create(payload) {
-  return testappAPI.post(`/rest-auth/password/change/`, payload.data)
+  return testappAPI.post(`/rest-auth/password/change/`, payload.data);
 }
+
 function rest_auth_password_reset_confirm_create(payload) {
-  return testappAPI.post(`/rest-auth/password/reset/confirm/`, payload.data)
+  return testappAPI.post(`/rest-auth/password/reset/confirm/`, payload.data);
 }
+
 function rest_auth_registration_verify_email_create(payload) {
-  return testappAPI.post(`/rest-auth/registration/verify-email/`, payload.data)
+  return testappAPI.post(`/rest-auth/registration/verify-email/`, payload.data);
 }
+
 export const apiService = {
   api_v1_login_create,
   api_v1_signup_create,
@@ -60,4 +82,4 @@ export const apiService = {
   rest_auth_password_change_create,
   rest_auth_password_reset_confirm_create,
   rest_auth_registration_verify_email_create
-}
+};
